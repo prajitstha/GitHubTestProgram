a4c6c2aa2dd872cbc76369bd1bc4cc47b82a37da
prazeet shrestha <prajit@coolvirgo.com>
2015-11-04 10:56:31 +0100

    add method A

---
+++ GitHubTestProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTestProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public void MethodA()
+        {
+            Console.WriteLine("'method A");
+        }
+    }
+}
